51d9671386f2001514c056e576b917df98e9d5ad
Manho Joung <MamboFamily2024@gmail.com>
2025-07-05 00:12:19 -0700

    Initial deploy to OCI 3

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,12 @@
+import os
 from .base import *
+from django.core.exceptions import ImproperlyConfigured
 
-DEBUG = False
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY environment variable is not set.")
+
+DEBUG = False  # Should be False in production!
 
 ALLOWED_HOSTS = ['127.0.0.1', '170.9.20.139', 'mambohealth.eu.org', 'www.mambohealth.eu.org']
 
