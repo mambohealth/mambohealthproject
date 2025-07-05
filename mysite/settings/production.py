2f1ce21669b9eacfb18d046d2a89641676dc1d64
Manho Joung <MamboFamily2024@gmail.com>
2025-07-04 23:59:14 -0700

    Initial deploy to OCI 2

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['127.0.0.1', '170.9.20.139', 'mambohealth.eu.org', 'www.mambohealth.eu.org']
+
 try:
     from .local import *
 except ImportError:
