20c69ae5d846ffc58942a9ba244226af74ed2c34
Manho Joung <MamboFamily2024@gmail.com>
2025-07-04 21:19:27 -0700

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rkof=ig_pmabyz^&z^=$6rxx%pbp1!u53wl1fxs8u_s$!t!!%q"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
