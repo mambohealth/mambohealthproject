1bbd42977c89dadc5f1a2b7d984b15ef8531a573
Manho Joung <MamboFamily2024@gmail.com>
2025-07-05 19:52:45 -0700

    Phase 1.3.2

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../templates/**/*.html',
-    '../../**/templates/**/*.html',
-    '../../**/*.js',
+    "../../**/templates/**/*.html",  // All HTML templates across apps
+    "../../templates/**/*.html",     // Global templates
+    "../../**/*.py",                 // Optional: scan for class names in Python (StreamFields, etc.)
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
 
